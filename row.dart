2f8f2a50591ad3029575af81eb7669b2f3df4915
fayezCoding <fayez.coder@gmail.com>
2022-08-09 05:49:29 +0300

    dart modified

--- row.dart
+++ row.dart
@@ -1,9 +1,5 @@
 Row(
           children: [
-            _iconContent(FontAwesomeIcons.twitter, () {}),
-            _iconContent(FontAwesomeIcons.github, () {}),
-            _iconContent(FontAwesomeIcons.tiktok, () {}),
-            _iconContent(FontAwesomeIcons.whatsapp, () {}),
-            _iconContent(FontAwesomeIcons.facebook, () {}),
-          ],
-        )
+            Text("hello")
+          ]
+        );
--- text.dart
+++ text.dart
@@ -1,10 +1,4 @@
- Widget _appBarButton(String title, VoidCallback onTap) {
-    // ignore: deprecated_member_use
-    return FlatButton(
-      onPressed: onTap,
-      child: Text(
-        title,
-        style: const TextStyle(color: Colors.white),
-      ),
-    );
-  }
+void main (){
+
+  
+}
