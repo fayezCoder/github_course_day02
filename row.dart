b5de6584f2a10628f87cfab9a7e50853b940bd28
fayezCoding <fayez.coder@gmail.com>
2022-08-09 05:20:12 +0300

    add 2 dart file

---
+++ row.dart
@@ -0,0 +1,9 @@
+Row(
+          children: [
+            _iconContent(FontAwesomeIcons.twitter, () {}),
+            _iconContent(FontAwesomeIcons.github, () {}),
+            _iconContent(FontAwesomeIcons.tiktok, () {}),
+            _iconContent(FontAwesomeIcons.whatsapp, () {}),
+            _iconContent(FontAwesomeIcons.facebook, () {}),
+          ],
+        )
